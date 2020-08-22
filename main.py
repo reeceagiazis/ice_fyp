584acb8d5f0638441ce90cedb77acf7e495501e5
Reece Agiazis <ragi3@student.monash.edu>
2020-08-22 13:51:44 +1000

    starting weather station api
    using email to get api key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np 
+import comms_routine as cr
 from picamera.array import PiRGBArray
 from picamera import PiCamera 
 
@@ -90,5 +91,7 @@ class create_windows():
 
 
 #main loop for script (like c)
+cr.Alert.send_email("r.agiazis@gmail.com", "hey")
+
 create_windows.start_id(640, 480, 15)
 create_windows.close_program()
