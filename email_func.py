13efa5608f230d2c15035c81fe631711dcabc0dd
Reece Agiazis <ragi3@student.monash.edu>
2020-08-22 12:45:29 +1000

    changed to init method for conventions

--- email_func.py
+++ email_func.py
@@ -1,7 +1,11 @@
 import smtplib, ssl
 
 class Alert(object):
-    def send_email(receiver_email, message):
+    def __init__(self, receiver_email, message):
+        self.receiver_email = receiver_email
+        self.message = message
+        
+    def send_email(self):
         port = 465  # For SSL
         password = "FYP2020COVID19"
         email_address = "icedetector.alert@gmail.com"
@@ -13,6 +17,8 @@ class Alert(object):
         with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
             server.login(email_address, password)
             try:
-                server.sendmail(sender_email, receiver_email, message)
+                server.sendmail(sender_email, self.receiver_email, self.message)
             except smtplib.SMTPRecipientsRefused:
-                print("Recipient refused: Are you sure " + receiver_email + " is the correct address?")
+                print("Recipient refused: Are you sure " + self.receiver_email + " is the correct address?")
+                
+Alert("r.agiazis@gmail.com", "hey").send_email()
