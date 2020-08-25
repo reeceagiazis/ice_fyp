9115f6c959bb170c3078465cd39e32deab3179b6
Reece Agiazis <ragi3@student.monash.edu>
2020-08-25 19:38:06 +1000

    added functionality to read choices from email
    going to allow us to put in more options and allow the user to communicate with the device over email

--- comms_routine.py
+++ comms_routine.py
@@ -1,5 +1,6 @@
-import email, smtplib, ssl
+import email, smtplib, ssl, imaplib, os, re
 from email import encoders
+from email.header import decode_header
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -12,6 +13,7 @@ class Alert(object):
         self.password = "FYP2020COVID19"
         email_address = "icedetector.alert@gmail.com"
         self.sender_email = email_address
+        self.username = email_address
     
     def send_email_file(self, filename):
         message = MIMEMultipart("alternative")
@@ -103,14 +105,77 @@ class Alert(object):
                 server.sendmail(sender_email, self.receiver_email, self.message)
             except smtplib.SMTPRecipientsRefused:
                 print("Recipient refused: Are you sure " + self.receiver_email + " is the correct address?")
-           
-
-# class Read(object):
-#     def __init__(self, receiver_email, message):
-#         self.receiver_email = receiver_email
-#         self.message = message
+                
+    def read(self):
+        imap = imaplib.IMAP4_SSL("imap.gmail.com")   
+        imap.login(self.username, self.password)
+        status, messages = imap.select("INBOX")
+        # number of top emails to fetch
+        N = 1
+        # total number of emails
+        messages = int(messages[0])
         
+        for i in range(messages, messages-N, -1):
+            # fetch the email message by ID
+            res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject = decode_header(msg["Subject"])[0][0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
+                    subject = subject.decode()
+                # email sender
+                from_ = msg.get("From")
+                #print("Subject:", subject)
+                #print("From:", from_)
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                else:
+                    # extract content type of email
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+
+                        
+                #print("="*100)
+                imap.close()
+                imap.logout()
+                
+                return subject, body
+            
+    def read_email_choice():
+        subject, option = Alert("icedetector.alert@gmail.com").read()
+        search = "choice"
+        
+        #splits the html div into an array
+        a = option.replace(">", " ",)
+        b = a.replace("<", " ")
+        option = b.split()
+
+        #only returns the choice if the subject is choice, else will return 0
+        if search in subject:
+            return option[2]
+        else:
+            print("No choice found in email")
+            return 0
+
+
         
         
 #Alert("r.agiazis@gmail.com", "Ice has been detected").send_email()
-Alert("icedetector.alert@gmail.com").send_email_file("image.jpg")
+#Alert("icedetector.alert@gmail.com").send_email_file("image.jpg")
+#Alert("icedetector.alert@gmail.com").read()
+Alert.read_email_choice()
